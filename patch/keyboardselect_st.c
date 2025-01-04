8b42b98aea3f70ef290db5d5b5b7fe02fab263df
DreamMaoMao <2523610504@qq.com>
2025-01-04 10:41:03 +0800

    optimize var name

--- patch/keyboardselect_st.c
+++ patch/keyboardselect_st.c
@@ -105,7 +105,7 @@ static const char *valid_char[] = {
 };
 
 int
-is_valid_char(Rune u) {
+is_valid_head_char(Rune u) {
 	int i;
 	for ( i = 0; i < LEN(valid_char); i++) {
 		if (u == *valid_char[i]) {
@@ -809,7 +809,7 @@ kbds_search_regex(void)
 		head = 0;
 		bottom = 0;
 		for (c.x = 0; c.x < c.len; c.x++) {
-			if(head_hit == 0 && bottom_hit == 0 && c.line[c.x].u != L' ' && is_valid_char(c.line[c.x].u)) {
+			if(head_hit == 0 && bottom_hit == 0 && c.line[c.x].u != L' ' && is_valid_head_char(c.line[c.x].u)) {
 				head = c.x;
 				head_hit = 1;
 			}
@@ -891,7 +891,7 @@ kbds_search_url(void)
 		head = 0;
 		bottom = 0;
 		for (c.x = 0; c.x < c.len; c.x++) {
-			if(head_hit == 0 && bottom_hit == 0 && c.line[c.x].u != L' ' && is_valid_char(c.line[c.x].u)) {
+			if(head_hit == 0 && bottom_hit == 0 && c.line[c.x].u != L' ' && is_valid_head_char(c.line[c.x].u)) {
 				head = c.x;
 				head_hit = 1;
 			}
